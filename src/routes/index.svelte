a95b1bf18a3a5c5df7a4e30f0fd5439fdce1003a
VanishMax <maxkors2014@gmail.com>
2020-06-19 10:56:58 +0900

    Change fonts of all text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,9 @@
 	</figure>
 
 	<h3>Max Korsunov</h3>
-	<p>The lover of the new</p>
+	<p>The lover of the new. Front-end developer with creative mind.</p>
+
+	<a href="/cv">See the CV</a>
 
 	<nav class="flex-center">
 		<a class="flex-center" href="https://github.com/VanishMax" target="_blank" rel="noreferrer nofollow">
@@ -28,16 +30,16 @@
 		position: relative;
 		margin: 5rem auto 0;
 		border-radius: 50px;
-		background: linear-gradient(145deg, #d1d1d1, #f8f8f8);
+		background: linear-gradient(145deg, #dfe1ec, #f8f8f8);
 		box-shadow: 7px 7px 14px #c7c8cd, -7px -7px 14px #ffffff;
-		width: 300px;
-		padding: 100px 30px 30px;
+		width: 350px;
+		padding: 100px 50px 40px;
 	}
 
 	figure {
 		position: absolute;
 		top: 0;
-		left: 30px;
+		left: 50px;
 		width: 150px;
 		height: 150px;
 		border-radius: 50%;
@@ -52,6 +54,37 @@
 		border-radius: 50%;
 	}
 
+	h3 {
+		font-size: 25px;
+	}
+
+	p {
+		line-height: 20px;
+	}
+
+	header > a {
+		display: inline-block;
+		margin-top: 10px;
+		margin-bottom: 30px;
+		width: auto;
+		text-decoration: none;
+		background-color: #e9eaf2;
+		color: #424567;
+		padding: 8px 15px;
+		border-radius: 9px;
+		box-shadow: 2px 2px 5px #c7c8cd, -3px -3px 6px #fff;
+		transition: box-shadow .2s;
+	}
+
+	header > a:hover {
+		box-shadow: 2px 3px 5px 2px #c7c8cd, -3px -3px 6px 2px #fff;
+		transition: box-shadow .2s;
+	}
+
+	header > a:active {
+		box-shadow: inset 2px 2px 5px #c7c8cd, inset -3px -3px 6px #ffffff;
+	}
+
 	nav {
 		justify-content: space-between;
 	}
@@ -62,6 +95,10 @@
 		border-radius: 50%;
 		background: linear-gradient(145deg, #d9d9d9, #fafafa);
 		box-shadow: inset 2px 2px 5px #c7c8cd, inset -3px -3px 6px #ffffff;
+	}
+
+	nav a:hover {
+		box-shadow: 2px 2px 5px #c7c8cd, -3px -3px 6px #fff;
 	}
 
 	nav img {
