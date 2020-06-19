c28af891c8f91d93770bc4a61507ae453b7debac
VanishMax <maxkors2014@gmail.com>
2020-06-19 14:37:53 +0900

    Add meta tags to article page

--- src/routes/_articles/about-me.js
+++ src/routes/_articles/about-me.js
@@ -4,11 +4,9 @@ const AboutMe = {
   cover: 'about-me-cover.jpg',
   date: 'June 20, 2020',
   lead: 'Since the beginning of learning HTML till creation of personal blog',
+  keywords: 'about me, school, university, innopolis, html, css, php, javascript',
   next: {slug: 'lolka', title: 'Поиграем в наши долгие названия длинных статей?'},
   prev: {slug: 'ввл', title: 'Поиграем в наши долгие названия длинных статей?'},
-  meta: {
-  
-  },
   contents: {},
 };
 
--- src/routes/articles/[slug].svelte
+++ src/routes/articles/[slug].svelte
@@ -13,6 +13,25 @@
   export let article;
 </script>
 
+<svelte:head>
+  <title>{article.title} | vanished</title>
+  <meta name="title" content={article.title + '| vanished'}>
+  <meta name="description" content={article.lead + '| vanished'}>
+  <meta name="keywords" content={(article.keywords ? article.keywords + ', ' : '') + 'Max Korsunov, vanishmax, developer, programmer, front-end'}>
+  <meta name="author" content="Max Korsunov">
+
+  <meta property="og:type" content="article">
+  <meta property="og:url" content={'https://vanishmax.com/articles/' + article.slug}>
+  <meta property="og:title" content={article.title + '| vanished'}>
+  <meta property="og:description" content={article.lead + '| vanished'}>
+  <meta property="og:image" content={'https://vanishmax.com/articles-img/' + article.cover}>
+
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content={article.title + '| vanished'}>
+  <meta name="twitter:description" content={article.lead + '| vanished'}>
+  <meta name="twitter:image" content={'https://vanishmax.com/articles-img/' + article.cover}>
+</svelte:head>
+
 <a href="/">
   <img src="/vanished.png" alt="vanished">
 </a>
