07dd66c382b52926bba9582625930d4dd31ebd94
vanishmax <maxkors2014@gmail.com>
2022-12-26 18:17:55 +0800

    fix: Change adapter to Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,8 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+    adapter: vercel({}),
+	},
 };
 
 export default config;
